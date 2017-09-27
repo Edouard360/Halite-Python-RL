696ef02bf6c97beb2f38b8f0ca54177d0b749613
Edouard360 <mailedouardmehlman@gmail.com>
2017-09-27 15:34:11 +0200

    Adding tests and docs, as well as the right compiler for halite file
    This change addresses the need by:
    
    * Create util.py to download games for testing purposes
    * Adding a template for .rst file
    * Adding test about the train folder to increase coverage
    * The right version of gcc compiler g++-4.9

--- tests/reward_test.py
+++ tests/reward_test.py
@@ -1,16 +1,42 @@
 """
 Tests the reward function
 """
-from train.reward import discount_rewards
+from train.reward import discount_rewards, rawRewards, allRewards
+from train.experience import ExperienceVanilla
+from train.worker import Worker
 import unittest
 
 import numpy as np
+from tests.util import game_states_from_url
 
 
 class TestReward(unittest.TestCase):
     def test_length_discount_rewards(self):
         self.assertTrue(len(discount_rewards(np.array([1]))) == 1)
         self.assertTrue(len(discount_rewards(np.array([1, 3]))) == 2)
+
+    def test_reward(self):
+        GAME_URL = 'https://s3.eu-central-1.amazonaws.com/halite-python-rl/hlt-games/trained-bot.hlt'
+        game_states, moves = game_states_from_url(GAME_URL)
+
+        raw_rewards = rawRewards(game_states)
+        self.assertTrue(len(raw_rewards) == len(game_states) - 1)
+
+        all_states, all_moves, all_rewards = allRewards(game_states, moves)
+        self.assertTrue(len(all_states) >= len(game_states) - 1)
+        self.assertTrue(len(all_moves) >= len(moves))
+        self.assertTrue(len(all_rewards) == len(all_moves) and len(all_states) == len(all_moves))
+        experience = ExperienceVanilla()
+        experience.add_episode(game_states, moves)
+        experience.add_episode(game_states, moves)
+        self.assertTrue(len(experience.moves) == 2 * len(all_moves))
+        batch_states, batch_moves, batch_rewards = experience.batch()
+        self.assertTrue(len(batch_rewards) == len(batch_moves) and len(batch_states) == len(batch_moves))
+
+    def test_worker(self):
+        worker = Worker(2000, 2, None)
+        self.assertTrue(worker.port == 2002)
+        worker.p.terminate()
 
 
 if __name__ == '__main__':
